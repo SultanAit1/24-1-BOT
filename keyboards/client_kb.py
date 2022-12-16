acc22e7ea31e2d226a5dd89f93643784956f1262
EsenbekM <esenbekm03@gmail.com>
2022-12-16 22:07:14 +0600

    lesson 3

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -15,3 +15,25 @@ share_contact = KeyboardButton("Share contact", request_contact=True)
 
 start_markup.add(start_button, info_button, quiz_button,
                  share_location, share_contact)
+
+cancel_button = KeyboardButton('CANCEL')
+cancel_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(cancel_button)
+
+gender_m = KeyboardButton("Мужчина")
+gender_f = KeyboardButton("Женщина")
+gender_u = KeyboardButton("Незнаю")
+
+gender_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(gender_m, gender_f, gender_u, cancel_button)
+
+
+submit_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(KeyboardButton('ДА'), KeyboardButton('НЕТ'))
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from aiogram.utils import executor
-from handlers import client, callback, extra, admin
+from handlers import client, callback, extra, admin, fsm_anketa
 from config import dp
 import logging
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
+fsm_anketa.register_handlers_fsm_anketa(dp)
 
 extra.register_handler_extra(dp)
 
