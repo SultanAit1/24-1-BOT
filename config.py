c518af5ca891f24a35750f78cccef44be4050cab
aitmanbetsul514@gmail.com <aitmanbetsul514@gmail.com>
2023-03-29 18:23:39 +0600

    бот рассылка

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 
 
-TOKEN = config("TOKEN")
+TOKEN=config('5651777397:AAFTiTgEuG46W-_Ps9WH3wj1JQD0IjB78hk')
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
