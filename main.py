38e080f2d0548ec8504de456bdaccd0d003563c7
aitmanbetsul514@gmail.com <aitmanbetsul514@gmail.com>
2023-03-29 18:41:11 +0600

    бот рассылка

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from aiogram import Bot, Dispatcher
+
+
 
 from handlers import client
 from config import dp
 import logging
 from aiogram.utils import executor
 import sqlite3
-
+from decouple import config
 client.register_handlers_client(dp)
 
 
+TOKEN=config('TOKEN')
 
+bot = Bot(TOKEN)
+dp = Dispatcher(bot=bot)
+ADMIN_ID = [661114436,]
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
